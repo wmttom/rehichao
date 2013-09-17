f6f59b8d39c66373e0d91f5a2a4adae26a0d43c6
wmttom <wmttom@gmail.com>
2013-09-17 12:51:03 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import distutils.core
+
+version = "0.0.1"
+
+distutils.core.setup(
+    name="rehichao",
+    version=version,
+    py_modules=["rehichao"],
+    author="",
+    author_email="",
+    url="https://github.com/wmttom/rehichao",
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    description="redis",
+    )
